467c36a3f77aff57929bbd6ed7007cdb9c33ae2d
RadonFreedom <radon.freedom@gmail.com>
2019-11-27 11:17:06 +0800

    1

---
+++ src/main/java/fre/shown/tryhook/TryhookApplication.java
@@ -0,0 +1,13 @@
+package fre.shown.tryhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryhookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fre/shown/tryhook/TryhookApplicationTests.java
@@ -0,0 +1,13 @@
+package fre.shown.tryhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryhookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
