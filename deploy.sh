77651e364a75eefe3716b2a825c7b1cfe8495379
RadonFreedom <radon.freedom@gmail.com>
2019-12-23 16:47:39 +0800

    增加本地profile

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mvn clean package
+sshfs root@101.132.157.78:/root ~/cloud
+cp target/tryhook-0.0.1-SNAPSHOT.jar ~/cloud
+ssh root@101.132.157.78
+ps -ef | grep java
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar tryhook-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
+
+# local
+java -jar tryhook-0.0.1-SNAPSHOT.jar --spring.profiles.active=local
