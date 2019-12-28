78050985e45f619a6ac14959c30aa2cde85a2e3a
RadonFreedom <radon.freedom@gmail.com>
2019-12-28 19:09:33 +0800

    修改部署命令

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd tryhook
 git pull
 mvn clean package
 ps -ef | grep java
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar tryhook-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar target/tryhook-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
 
 # local
 # java -jar tryhook-0.0.1-SNAPSHOT.jar --spring.profiles.active=local
